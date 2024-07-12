fc1b860effb93225902140afc68ad095a639ceef
DevNadimm <nadimchowdhury87@gmail.com>
2024-07-12 20:09:32 +0600

    Custom App bar created

---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+import '../widgets/custom_app_bar.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Column(
+          children: [
+            CustomAppBar(),
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/widgets/custom_app_bar.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+
+class CustomAppBar extends StatelessWidget {
+  CustomAppBar({super.key});
+
+  final OutlineInputBorder border = OutlineInputBorder(
+    borderRadius: const BorderRadius.horizontal(
+      left: Radius.circular(30),
+    ),
+    borderSide: BorderSide(
+      width: 1.8,
+      color: const Color(0xFF003366).withOpacity(0.5),
+    ),
+  );
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        const Padding(
+          padding: EdgeInsets.symmetric(
+            horizontal: 20,
+            vertical: 10,
+          ),
+          child: Text(
+            'Shoes \nCollection',
+            style: TextStyle(
+              color: Color(0xFF003366),
+              fontSize: 30,
+              fontWeight: FontWeight.w700,
+              fontFamily: 'Poppins',
+            ),
+          ),
+        ),
+        Expanded(
+          child: TextField(
+            decoration: InputDecoration(
+                hintText: 'Search',
+                hintStyle: TextStyle(
+                  fontWeight: FontWeight.w600,
+                  color: const Color(0xFF003366).withOpacity(0.7),
+                ),
+                prefixIcon: Icon(
+                  Icons.search,
+                  color: const Color(0xFF003366).withOpacity(0.7),
+                ),
+                border: border,
+                focusedBorder: border,
+                enabledBorder: border,
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
